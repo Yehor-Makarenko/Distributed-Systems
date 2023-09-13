9e81f880d76305d11f9c267e8e163190398abe1c
Yehor Makarenko <mak.yehor.knu@gmail.com>
2023-09-13 20:15:04 +0300

    first commit

---
+++ Lab1/src/ProgrammA.java
@@ -0,0 +1,80 @@
+import javax.swing.*;
+
+public class ProgrammA {
+  public static void main(String[] args) {    
+    JFrame f = new JFrame("Lab1");
+    JButton startButton = new JButton("Start");    
+    JSlider slider = new JSlider(0, 0, 100, 50); 
+    JPanel panel = new JPanel(null);
+    JSpinner spinnerMinus = new JSpinner(new SpinnerNumberModel(5, 1, 10, 1));
+    JSpinner spinnerPlus = new JSpinner(new SpinnerNumberModel(5, 1, 10, 1));
+    Thread tMinus = new Thread(() -> { 
+      while(true) {
+        synchronized(slider) {     
+          try {
+            Thread.sleep(10);
+          } catch (InterruptedException e1) {
+            break;
+          }     
+          if(slider.getValue() > 10) {
+            slider.setValue(slider.getValue() - 1);
+          }
+        }        
+      }
+    });
+    Thread tPlus = new Thread(() -> { 
+      while(true) {               
+        synchronized(slider) {  
+          try {
+            Thread.sleep(10);
+          } catch (InterruptedException e1) {
+            break;
+          }         
+          if(slider.getValue() < 90) {
+            slider.setValue(slider.getValue() + 1);
+          }
+        }        
+      }
+    });   
+
+    startButton.addActionListener(e -> {
+      if (tMinus.isAlive()) {
+        return;
+      }
+      
+      tMinus.setDaemon(true);
+      tPlus.setDaemon(true);
+      tMinus.start();
+      tPlus.start();
+    });  
+
+    spinnerMinus.addChangeListener(e -> {
+      tMinus.setPriority((int)spinnerMinus.getValue());      
+    });
+
+    spinnerPlus.addChangeListener(e -> {
+      tPlus.setPriority((int)spinnerPlus.getValue());
+    });
+
+    f.setSize(500, 800);           
+    spinnerMinus.setBounds(100, 150, 50, 30);
+    spinnerPlus.setBounds(350, 150, 50, 30);
+    startButton.setBounds(200, 150, 100, 30);    
+    slider.setBounds(50, 70, 400, 50);
+
+    slider.setMinorTickSpacing(5);
+    slider.setMajorTickSpacing(10);
+    slider.setPaintTicks(true);
+    slider.setPaintLabels(true); 
+  
+    panel.add(slider);
+    panel.add(spinnerMinus);
+    panel.add(startButton); 
+    panel.add(spinnerPlus);   
+    f.setContentPane(panel);
+    
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    f.setLayout(null);
+    f.setVisible(true);
+  } 
+}
---
+++ Lab1/src/ProgrammB.java
@@ -0,0 +1,111 @@
+import javax.swing.*;
+
+public class ProgrammB {
+  private static int semaphore = 1;
+
+  public static void main(String[] args) {           
+    JFrame f = new JFrame("Lab1");
+    JButton startButton1 = new JButton("Start1");    
+    JButton startButton2 = new JButton("Start2");    
+    JButton stopButton1 = new JButton("Stop1");    
+    JButton stopButton2 = new JButton("Stop2");    
+    JSlider slider = new JSlider(0, 0, 100, 50); 
+    JPanel panel = new JPanel(null);    
+    Thread[] threads = new Thread[2];                
+
+    startButton1.addActionListener(e -> {
+      if (semaphore == 0) {
+        if (threads[1] != null) {
+          System.out.println("Another thread is working");
+        }        
+        return;
+      }
+
+      threads[0] = new Thread(() -> { 
+        while(!Thread.interrupted()) {          
+          try {
+            Thread.sleep(10);
+          } catch (InterruptedException e1) {
+            break;
+          }
+          if(slider.getValue() > 10) {
+            slider.setValue(slider.getValue() - 1);
+          }          
+        }
+      });
+
+      threads[0].setPriority(Thread.MIN_PRIORITY);      
+      semaphore = 0;
+      threads[0].start();
+    });
+
+    startButton2.addActionListener(e -> {
+      if (semaphore == 0) {
+        if (threads[0] != null) {
+          System.out.println("Another thread is working");
+        }        
+        return;
+      }
+
+      threads[1] = new Thread(() -> { 
+        while(!Thread.interrupted()) {
+          try {
+            Thread.sleep(10);
+          } catch (InterruptedException e1) {
+            break;
+          }
+          if(slider.getValue() < 90) {
+            slider.setValue(slider.getValue() + 1);
+          }          
+        }
+      });
+
+      threads[1].setPriority(Thread.MAX_PRIORITY);      
+      semaphore = 0;
+      threads[1].start();
+    });
+
+    stopButton1.addActionListener(e -> {
+      if (threads[0] == null) {
+        return;
+      }
+
+      threads[0].interrupt();    
+      threads[0] = null;  
+      semaphore = 1;
+    });
+
+    stopButton2.addActionListener(e -> {
+      if (threads[1] == null) {
+        return;
+      }
+
+      threads[1].interrupt();   
+      threads[1] = null;   
+      semaphore = 1;
+    });
+
+    f.setSize(500, 800);           
+    startButton1.setBounds(100, 150, 100, 30);    
+    startButton2.setBounds(300, 150, 100, 30);    
+    stopButton1.setBounds(100, 200, 100, 30);    
+    stopButton2.setBounds(300, 200, 100, 30);        
+    slider.setBounds(50, 70, 400, 50);
+
+    slider.setMinorTickSpacing(5);
+    slider.setMajorTickSpacing(10);
+    slider.setPaintTicks(true);
+    slider.setPaintLabels(true);    
+  
+    panel.add(slider);    
+    panel.add(startButton1); 
+    panel.add(startButton2);     
+    panel.add(stopButton1);
+    panel.add(stopButton2);
+    f.setContentPane(panel);
+    
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    f.setLayout(null);
+    f.setVisible(true);
+  } 
+}
